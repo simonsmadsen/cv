78acc1da21b504211c05d389233735229e6cbe48
Simon Madsen <sism@tv2.dk>
2022-11-03 15:14:41 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,160 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-900 absolute inset-0 p-10 overflow-scroll">
+      <h1 className="text-white text-3xl">
+        Hi! I'm Simon Madsen 🙋🏼‍♂️ - Software Developer
+        <br /> ⌨
+      </h1>
+      <p className="text-gray-200  mt-2 text-sm max-w-3xl leading-6">
+        Hello there, and welcome to my website. My name is Simon. I see myself
+        as a passionated programmer. I have been programming in many languages
+        and varied my programming style several times, over the years <br />
+        Programming is a hobby to me. I love to explore new ideas, languages &
+        discuss different solutions in my free time . In my opinion, we should
+        try to, challenge the defaults and explore new ways to solve problems.
+      </p>
+      <h2 className="mt-5 text-gray-200 text-lg font-semibold">
+        My Personal stuff 🥳
+      </h2>
+      <p className="text-gray-200 text-sm max-w-3xl leading-6">
+        I'm living in Odense with my girlfriend and our little girl Ella. I
+        enjoy going to the gym first thing in the morning. The rest of my free
+        time is used with my family and friends.
+      </p>
+      <h2 className="mt-5 text-gray-200 text-lg font-semibold">
+        My current Hobby stack 🏡
+      </h2>
+      <p className="text-gray-200 mt-1 text-sm max-w-3xl leading-6">
+        📱 React Native - with expo.
+        <br /> 💻 Next JS - with typescript.
+        <br /> 🦀 Rust - with Async Grapql, database from planetscale(good old
+        mysql) & hosted at fargate aws. <br />
+        🔨 Github action as pipeline/gitops. <br />
+        👨🏼‍🎨 Tailwindcss ❤️
+        <br />
+        All clients using codegen, to generate types for the rust graphql
+        server.
+      </p>
+      <h2 className="mt-5 text-gray-200 text-lg font-semibold">
+        My current stack at work (TV2)🧑🏼‍💻
+      </h2>
+      <p className="text-gray-200 mt-1 text-sm max-w-3xl leading-6">
+        💻 Next JS - with typescript.
+        <br />
+        📟 Node JS - Apollo Graphql <br />
+        Kubernets - in aws eks. <br />
+        Argo - for pod deployments. <br />
+        github action
+        <br />
+        tailwindcss
+      </p>
+      <h2 className="mt-5 text-gray-200 text-lg font-semibold">My utils ⚒️</h2>
+      <p className="text-gray-200 text-sm max-w-3xl leading-6">
+        Macbook - VSCode - Vim Motion - gruvbox theme
+      </p>
+      <h2 className="mt-5  text-gray-200 text-2xl font-semibold">
+        Here is a brief overview over my programming journey
+      </h2>
+      <ol class="relative mt-2 border-l border-gray-200 dark:border-gray-700 ml-[-1rem]">
+        <li class="mb-10 ml-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            now
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            Play Web & Big screens TV2
+          </h3>
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            Enjoying being a part of the PWBS Teams at TV2 working on
+            play.tv2.dk, checkout.tv2.dk, mit.tv2.dk & help build a brand new
+            app for Samsung and LG smart TVs. <br />
+          </p>
+        </li>
+        <li class="mb-10 ml-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            2020
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            Codespace
+          </h3>
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            Starting my own Business with a co-worker from Exotech.
+            <br /> Building Mobile Application using React Native and AWS
+            Serverless.
+            <br /> <br />
+            <span className="font-bold">Biggest projects:</span> <br />
+            Thirsty: Mobile Application to help nightclub owners promote, sell
+            and engage with their customers <br />
+            KeepItSimple: Mobile Application to help people fight addictions and
+            find open meetings. <br />
+            Kintella: Web Application to help nursing homes to connect next of
+            kin & their residents.
+          </p>
+        </li>
+        <li class="mb-10 ml-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            2018 exotech done - appinto done - Codespace start
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            Exotech Delphi & Appinto
+          </h3>
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"></p>
+        </li>
+        <li class="mb-10 ml-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            2017
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            Exotech Delphi & Appinto
+          </h3>
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"></p>
+        </li>
+        <li class="mb-10 ml-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            2016
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            EQS
+          </h3>
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"></p>
+        </li>
+        <li class="mb-10 ml-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            2015
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            Computer Science Specialist AP Degree
+          </h3>
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            Writing C# in school and making Laravel sites as a side hustle
+          </p>
+        </li>
+        <li class="mb-10 ml-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            2013
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            Webintegrator (PHP, AJAX, Codeigniter)
+          </h3>
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            Programming websites in basic HTML, CSS, and PHP. WordPress &
+            Codeigniter all day.
+          </p>
+        </li>
+        <li class="mb-10 ml-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            2011
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white"></h3>
+        </li>
+      </ol>
     </div>
   );
 }
-
-export default App;
